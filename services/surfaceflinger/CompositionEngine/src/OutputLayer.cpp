6a4eaa85f2dd8bfb1dae26c83a3b53bde1ade7b6
Huihong Luo <huisinro@google.com>
2023-08-18 14:26:32 -0700

    Fix a crash for skipped layers with null buffers
    No need to process these layers.
    
    Bug: 240653315
    Bug: 230073351
    Test: YouTube PIP at home screen
    Change-Id: Iced392706e33a2241197050ddd24e0fffaa3a723

--- services/surfaceflinger/CompositionEngine/src/OutputLayer.cpp
+++ services/surfaceflinger/CompositionEngine/src/OutputLayer.cpp
@@ -672,6 +672,9 @@ void OutputLayer::uncacheBuffers(const std::vector<uint64_t>& bufferIdsToUncache
 void OutputLayer::writeBufferStateToHWC(HWC2::Layer* hwcLayer,
                                         const LayerFECompositionState& outputIndependentState,
                                         bool skipLayer) {
+    if (skipLayer && outputIndependentState.buffer == nullptr) {
+        return;
+    }
     auto supportedPerFrameMetadata =
             getOutput().getDisplayColorProfile()->getSupportedPerFrameMetadata();
     if (auto error = hwcLayer->setPerFrameMetadata(supportedPerFrameMetadata,
