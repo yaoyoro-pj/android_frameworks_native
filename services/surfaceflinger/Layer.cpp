bbeb4ff70285f4656707ed4bceba88f905a46fc2
jiayongqiang <jiayongqiang@xiaomi.com>
2023-11-14 17:55:02 +0800

    Remove redundant calls and correct some comments.
    1. Remove first initiation of dataspace in the constructor of
       RequestedLayerState;
    2. snapshot->stretchEffect has been set in the approach of
       prepareBasicGeometryCompositionState(), so remove the second call.
    
    Change-Id: I784223ddce80756dc88ec78e8f0deb042e6389db
    Signed-off-by: jiayongqiang <jiayongqiang@xiaomi.com>

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -4241,7 +4241,6 @@ void Layer::updateSnapshot(bool updateGeometry) {
         prepareBasicGeometryCompositionState();
         prepareGeometryCompositionState();
         snapshot->roundedCorner = getRoundedCornerState();
-        snapshot->stretchEffect = getStretchEffect();
         snapshot->transformedBounds = mScreenBounds;
         if (mEffectiveShadowRadius > 0.f) {
             snapshot->shadowSettings = mFlinger->mDrawingState.globalShadowSettings;
