06c58c82291a0834e242ddba4881c42126a847dc
fukiame <fukiame@proton.me>
2026-03-10 14:28:33 +0700

    Revert "Allow unsafe operations in unsafe functions in generated code."
    This reverts commit f2ac825152e881ea2606d3d1ef8acbcb62c56b70.

--- libs/input/input_verifier.rs
+++ libs/input/input_verifier.rs
@@ -32,7 +32,6 @@ use bitflags::bitflags;
 use log::info;
 
 #[cxx::bridge(namespace = "android::input")]
-#[allow(unsafe_op_in_unsafe_fn)]
 mod ffi {
     #[namespace = "android"]
     unsafe extern "C++" {
